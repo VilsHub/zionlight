8190dcd1b4d95aee1362c8ea955340066a33bbde
Starlyvil <starlyvil@gmail.com>
2021-11-26 00:47:06 +0100

    Updated /app/applications.php files

--- config/applications.php
+++ config/applications.php
@@ -4,26 +4,7 @@ $vendorSpace            = $config->vendorDir."/".$name;
 $applicationDataSpace   = $config->applicationDataDir."/".$name;
 $applications = [
     // Applications that are dependent on http request, use 1st segment uri value as application ID
-    "zdash" => [
-        "config"        => [
-            "vendor"    => [
-                "socket"        => $vendorSpace."/http/routes/socket.php",
-                "content"       => $vendorSpace."/http/routes/content.php",
-                "webHandler"    => $vendorSpace."/http/handlers/web.php",
-                "apiHandler"    => $vendorSpace."/http/handlers/api.php",
-                "display"       => $vendorSpace."/display",
-            ],
-            "system"    => [
-                "socket"        => $applicationDataSpace."/http/routes/socket.php",
-                "content"       => $applicationDataSpace."/http/routes/content.php",
-                "webHandler"    => $applicationDataSpace."/http/handlers/web.php",
-                "apiHandler"    => $applicationDataSpace."/http/handlers/api.php",
-                "display"       => $applicationDataSpace."/display",
-            ]
-        ],
-        "apiID"         => "api",
-        "configInUse"   => "vendor"
-    ]
+    
 ];
 return $applications;
 ?>
