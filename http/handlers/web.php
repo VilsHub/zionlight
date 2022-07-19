b07e6aae29e6e38dfa7b7e49cd8f183f7a5d8d99
Starlyvil <starlyvil@gmail.com>
2022-07-19 21:38:31 +0100

    updated miss files for last commit

--- http/handlers/web.php
+++ http/handlers/web.php
@@ -3,27 +3,27 @@ use vilshub\http\Request;
 
 //Set blocks
 if(Request::$uri == $root){
-    
-    $block = $config->displayDir."/home/mainPublic.php";
-    $baseDir = $config->displayDir."/home/".$config->contentsFolder->load;
+
+    $block      = $app->getDisplayBlock("home", "mainPublic.php");
+    $baseDir    = $app->getLoadBase("home");
     $app->router->listen($block, $baseDir);
 
 }else if(Route::block($maintenance, Request::$uri)){
 
-    $block = $config->displayDir."/maintenance/maintenance.php";
-    $baseDir = $config->displayDir."/maintenance/".$config->contentsFolder->load;
+    $block      = $app->getDisplayBlock("maintenance", "maintenance.php");
+    $baseDir    = $app->getLoadBase("maintenance");
     $app->router->listen($block, $baseDir);
 
 }else if(Route::block($error, Request::$uri)){
 
-    $block = $config->displayDir."/error/error.php";
-    $baseDir = $config->displayDir."/error/".$config->contentsFolder->load;
+    $block      = $app->getDisplayBlock("error", "error.php");
+    $baseDir    = $app->getLoadBase("error");
     $app->router->listen($block, $baseDir);
 
 }else{//any other route
 
-    $block = $config->displayDir."/home/mainPublic.php";
-    $baseDir = $config->displayDir."/home/".$config->contentsFolder->load;
+    $block      = $app->getDisplayBlock("home", "mainPublic.php");
+    $baseDir    = $app->getLoadBase("home");
     $app->router->listen($block, $baseDir);
     
 }
